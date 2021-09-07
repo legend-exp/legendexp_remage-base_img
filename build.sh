c4f6f003074334e2e61fcdb989a77a0f2825b367
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2021-09-07 12:35:14 +0000

    Trying to fix locale issues

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 docker build -t gipert/remage-base:slim -f Dockerfile.slim .
-docker push gipert/remage-base:slim &
+docker push gipert/remage-base:slim
 
 docker build -t gipert/remage-base:G4.10.7 -f Dockerfile.G4-10-7 .
-docker push gipert/remage-base:G4.10.7 &
+docker push gipert/remage-base:G4.10.7
+
+docker build --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
+             -t gipert/remage-base:slim-debuginfo -f Dockerfile.slim .
+# docker push gipert/remage-base:slim-debuginfo
+
+docker build --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
+             -t gipert/remage-base:G4.10.7-debuginfo -f Dockerfile.G4-10-7 .
+# docker push gipert/remage-base:G4.10.7-debuginfo
 
 docker build -t gipert/remage-base:latest .
 docker push gipert/remage-base:latest
