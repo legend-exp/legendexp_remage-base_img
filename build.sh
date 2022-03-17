0cd0b11373e43d6f942fe2d90c6fbf574fd0a392
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-03-17 16:47:03 +0000

    Update G4 env variables, improve build script

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-docker build -t gipert/remage-base:slim -f Dockerfile.slim .
+docker build --pull --rm -t gipert/remage-base:slim -f Dockerfile.slim .
 docker push gipert/remage-base:slim
 
-docker build -t gipert/remage-base:G4.10.7 -f Dockerfile.G4-10-7 .
+docker build --pull --rm -t gipert/remage-base:G4.10.7 -f Dockerfile.G4-10-7 .
 docker push gipert/remage-base:G4.10.7
 
-docker build -t gipert/remage-base:G4.11.0 -f Dockerfile.G4-11-0 .
+docker build --pull --rm -t gipert/remage-base:G4.11.0 -f Dockerfile.G4-11-0 .
 docker push gipert/remage-base:G4.11.0
 
-docker build --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
+docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:slim-debuginfo -f Dockerfile.slim .
 # docker push gipert/remage-base:slim-debuginfo
 
-docker build --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
+docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:G4.10.7-debuginfo -f Dockerfile.G4-10-7 .
 # docker push gipert/remage-base:G4.10.7-debuginfo
 
-docker build --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
+docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:G4.11.0-debuginfo -f Dockerfile.G4-11-0 .
 # docker push gipert/remage-base:G4.11.0-debuginfo
 
-docker build -t gipert/remage-base:latest .
+docker build --pull --rm -t gipert/remage-base:latest .
 docker push gipert/remage-base:latest
