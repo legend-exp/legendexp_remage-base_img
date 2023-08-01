8e6b2d824e9eef60219325b9740672299d246e91
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-08-01 21:14:06 +0200

    Update G4 version to 11.1.2

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 docker build --pull --rm -t gipert/remage-base:slim -f Dockerfile.slim .
-docker push gipert/remage-base:slim
+docker push gipert/remage-base:slim &
 
 docker build --pull --rm -t gipert/remage-base:G4.10.7 -f Dockerfile.G4-10-7 .
-docker push gipert/remage-base:G4.10.7
+docker push gipert/remage-base:G4.10.7 &
 
 docker build --pull --rm -t gipert/remage-base:G4.11.0 -f Dockerfile.G4-11-0 .
-docker push gipert/remage-base:G4.11.0
+docker push gipert/remage-base:G4.11.0 &
 
 docker build --pull --rm -t gipert/remage-base:G4.11.1 -f Dockerfile.G4-11-1 .
-docker push gipert/remage-base:G4.11.1
+docker push gipert/remage-base:G4.11.1 &
 
 docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:slim-debuginfo -f Dockerfile.slim .
