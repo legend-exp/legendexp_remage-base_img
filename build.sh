b977f0daf65faadcce7706ee81e4ff585598adcd
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2024-11-04 18:02:47 +0100

    Bump remage version and fix container building logic

--- build.sh
+++ build.sh
@@ -21,5 +21,4 @@ docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
 docker build --pull --rm -t gipert/remage-base:latest .
 docker push gipert/remage-base:latest
 
-cd remage
-./build.sh
+cd remage && ./build.sh
--- remage/build.sh
+++ remage/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-docker build --pull --rm -t gipert/remage:latest .
-docker push gipert/remage:latest &
+_rmg_version="v0.2.0"
 
-docker build --pull --rm --build-arg REMAGE_BASE_FLAVOR="slim" -t gipert/remage:slim .
+docker build --pull --rm --build-arg REMAGE_VERSION="$_rmg_version" \
+             -t gipert/remage:"$_rmg_version" .
+docker push gipert/remage:"$_rmg_version"
+
+docker tag gipert/remage:"$_rmg_version" gipert/remage:latest
+docker push gipert/remage:latest 
+
+docker build --pull --rm \
+    --build-arg REMAGE_VERSION="$_rmg_version" \
+    --build-arg REMAGE_BASE_FLAVOR="slim" \
+    -t gipert/remage:slim .
 docker push gipert/remage:slim
