39eda856a0a906c7683c82d8473766c8282f203b
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2024-12-09 11:01:23 +0100

    Add Dockerfile for G4 v11.3

--- build.sh
+++ build.sh
@@ -3,20 +3,11 @@
 docker build --pull --rm -t gipert/remage-base:slim -f Dockerfile.slim .
 docker push gipert/remage-base:slim &
 
-# docker build --pull --rm -t gipert/remage-base:G4.10.7 -f Dockerfile.G4-10-7 .
-# docker push gipert/remage-base:G4.10.7 &
-
-# docker build --pull --rm -t gipert/remage-base:G4.11.0 -f Dockerfile.G4-11-0 .
-# docker push gipert/remage-base:G4.11.0 &
-
-# docker build --pull --rm -t gipert/remage-base:G4.11.1 -f Dockerfile.G4-11-1 .
-# docker push gipert/remage-base:G4.11.1 &
-
-docker build --pull --rm -t gipert/remage-base:G4.11.2 -f Dockerfile.G4-11-2 .
-docker push gipert/remage-base:G4.11.2 &
+docker build --pull --rm -t gipert/remage-base:G4.11.3 -f Dockerfile.G4-11-3 .
+docker push gipert/remage-base:G4.11.3 &
 
 docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
-             -t gipert/remage-base:G4.11.2-debuginfo -f Dockerfile.G4-11-2 .
+             -t gipert/remage-base:G4.11.3-debuginfo -f Dockerfile.G4-11-3 .
 
 docker build --pull --rm -t gipert/remage-base:latest .
 docker push gipert/remage-base:latest
