1f7f5c56dbd60a865ad8dfe9df2bcc99a802dd7a
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2024-12-18 16:20:22 +0100

    Adopt latest and stable tags

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-docker build --pull --rm -t gipert/remage-base:slim -f Dockerfile.slim .
-docker push gipert/remage-base:slim &
-
-docker build --pull --rm -t gipert/remage-base:G4.11.3 -f Dockerfile.G4-11-3 .
-docker push gipert/remage-base:G4.11.3 &
+for flavor in {slim,G4-11-2,G4-11-3,latest,stable}; do
+    docker_tag=$(echo $flavor | sed 's|-|.|g')
+    docker build --pull --rm -t gipert/remage-base:$docker_tag -f Dockerfile.$flavor . && \
+        docker push gipert/remage-base:$docker_tag
+done
 
 docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:G4.11.3-debuginfo -f Dockerfile.G4-11-3 .
-
-docker build --pull --rm -t gipert/remage-base:latest .
-docker push gipert/remage-base:latest
