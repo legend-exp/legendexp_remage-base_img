40d6687011039a609a80085dffdcad1b247ad0b0
Manuel Huber <ManuelHu@users.noreply.github.com>
2024-12-20 14:44:42 +0100

    Add G4.11.3-vecgeom-experimental variant to build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ done
 
 docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:G4.11.3-debuginfo -f Dockerfile.G4-11-3 .
+
+docker build --pull --rm --build-arg VECGEOM_VERSION="master" \
+             --build-arg GEANT4_USE_USOLIDS="ON" \
+             -t gipert/remage-base:G4.11.3-vecgeom-experimental -f Dockerfile.G4-11-3 .
