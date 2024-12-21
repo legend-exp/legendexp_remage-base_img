93ba496f4c86475f10ca1bc2732fa0f2ea2083ff
Manuel Huber <ManuelHu@users.noreply.github.com>
2024-12-21 16:58:11 +0100

    add missing docker push

--- build.sh
+++ build.sh
@@ -6,9 +6,15 @@ for flavor in {slim,G4-11-2,G4-11-3,latest,stable}; do
         docker push gipert/remage-base:$docker_tag
 done
 
+# special images, produced only for latest version
+flavor="G4-11-3"
+docker_tag=$(echo $flavor | sed 's|-|.|g')
+
 docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
-             -t gipert/remage-base:G4.11.3-debuginfo -f Dockerfile.G4-11-3 .
+             -t gipert/remage-base:${docker_tag}-debuginfo -f Dockerfile.$flavor . && \
+    docker push gipert/remage-base:$docker_tag-debuginfo
 
 docker build --pull --rm --build-arg VECGEOM_VERSION="master" \
              --build-arg GEANT4_USE_USOLIDS="ON" \
-             -t gipert/remage-base:G4.11.3-vecgeom-experimental -f Dockerfile.G4-11-3 .
+             -t gipert/remage-base:${docker_tag}-vecgeom-experimental -f Dockerfile.$flavor . && \
+    docker push gipert/remage-base:$docker_tag-vecgeom-experimental
