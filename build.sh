58167de5d72454e5138a6078162a56722b0c0508
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-03-18 12:01:41 +0100

    install pre-commit with uv

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ docker build --pull --rm --build-arg CMAKE_BUILD_TYPE="RelWithDebInfo" \
              -t gipert/remage-base:${docker_tag}-debuginfo -f Dockerfile.$flavor . && \
     docker push gipert/remage-base:$docker_tag-debuginfo
 
-docker build --pull --rm --build-arg VECGEOM_VERSION="master" \
-             --build-arg GEANT4_USE_USOLIDS="ON" \
-             -t gipert/remage-base:${docker_tag}-vecgeom-experimental -f Dockerfile.$flavor . && \
-    docker push gipert/remage-base:$docker_tag-vecgeom-experimental
+# docker build --pull --rm --build-arg VECGEOM_VERSION="master" \
+#              --build-arg GEANT4_USE_USOLIDS="ON" \
+#              -t gipert/remage-base:${docker_tag}-vecgeom-experimental -f Dockerfile.$flavor . && \
+#     docker push gipert/remage-base:$docker_tag-vecgeom-experimental
